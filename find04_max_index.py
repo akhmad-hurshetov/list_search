c9b888845e904783411423b3e38972af383fb011
Akhmad <akhmadhurshetov@gmail.com>
2022-11-09 20:13:41 +0500

    04

--- find04_max_index.py
+++ find04_max_index.py
@@ -5,4 +5,10 @@ def find_max_index(data):
         data: list of numbers
     returns: index of maximum number in the list
     """
-    return 0
+    idx=0
+    m=data[idx]
+    for i in range(len(data)):
+        if data[i]<m:
+            m=data[i]
+            idx=i    
+    return idx
